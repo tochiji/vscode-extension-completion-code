494b0c02605308918e2ad13ac0e67c271cef3c14
tochiji <7910558+tochiji@users.noreply.github.com>
2024-05-27 23:33:56 +0900

    .

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,13 @@
 import * as vscode from 'vscode';
 import { completionCodeClaude } from './claude';
+import { completionCodeGemini } from './gemini';
 import { completionCodeOpenAI } from './openai';
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('inline-completions started');
 
 	vscode.commands.registerCommand("vscode.contextmenu.completion-code-openai", completionCodeOpenAI);
+	vscode.commands.registerCommand("vscode.contextmenu.completion-code-gemini", completionCodeGemini);
 	vscode.commands.registerCommand("vscode.contextmenu.completion-code-claude", completionCodeClaude);
 }
 
